a2061a5f4e774c83d7935c9b419948b3084d6849
derenlei <derenlei@microsoft.com>
2025-01-30 01:09:06 -0800

    clean up

--- factcg/inference.py
+++ factcg/inference.py
@@ -58,8 +58,7 @@ class Inferencer():
         hypo_sents_mat = []
 
         for one_pre, one_hypo in zip(premise, hypo):
-            one_pre_sents = self.chunking_src_mop(one_pre)
-            # one_pre_sents = self.chunk_src_minicheck(one_pre)
+            one_pre_sents = self.chunking_src(one_pre)
             one_hypo_sents = [one_hypo] * len(one_pre_sents)
 
             premise_sent_mat.extend(one_pre_sents)
@@ -75,88 +74,21 @@ class Inferencer():
 
         output_score_all = []
         best_chunk_ids = []
-        output_score_per_example = torch.split(output_score_all_flat, [
-                                               # , dim=1)
-                                               math.prod(n) for n in chunksize_per_example])
+        output_score_per_example = torch.split(output_score_all_flat, [math.prod(n) for n in chunksize_per_example])
         for output_score, chunk_size in zip(output_score_per_example, chunksize_per_example):
             hypo_len, premise_len = chunk_size
             best_chunk_id = output_score.view(
                 hypo_len, premise_len).max(dim=1).indices[0].item()
-            output_score_val = output_score.view(hypo_len, premise_len).max(
-                # sum or mean depends on the task/aspect
-                dim=1).values.mean().item()
+            output_score_val = output_score.view(hypo_len, premise_len).max(dim=1).values.mean().item()
             output_score_all.append(output_score_val)
             best_chunk_ids.append(best_chunk_id)
         return output_score_all, best_chunk_ids
 
-    def chunk_src_minicheck(self, src, max_chunk_size=500):
-        def sent_tokenize_with_newlines(text):
-            blocks = text.split('\n')
-
-            tokenized_blocks = [sent_tokenize(block) for block in blocks]
-            tokenized_text = []
-            for block in tokenized_blocks:
-                tokenized_text.extend(block)
-                tokenized_text.append('\n')
-
-            return tokenized_text[:-1]
-
-        def chunks(lst, n):
-            """Yield successive chunks from lst with each having approximately n tokens.
-
-            For flan-t5, we split using the white space;
-            For roberta and deberta, we split using the tokenization.
-            """
-            if "t5" in self.model_name:
-                current_chunk = []
-                current_word_count = 0
-                for sentence in lst:
-                    sentence_word_count = len(sentence.split())
-                    if current_word_count + sentence_word_count > n:
-                        yield ' '.join(current_chunk)
-                        current_chunk = [sentence]
-                        current_word_count = sentence_word_count
-                    else:
-                        current_chunk.append(sentence)
-                        current_word_count += sentence_word_count
-                if current_chunk:
-                    yield ' '.join(current_chunk)
-            else:
-                current_chunk = []
-                current_token_count = 0
-                for sentence in lst:
-                    sentence_word_count = len(self.tokenizer(
-                        sentence, padding=False, add_special_tokens=False,
-                        max_length=self.max_model_len, truncation=True)['input_ids'])
-                    if current_token_count + sentence_word_count > n:
-                        yield ' '.join(current_chunk)
-                        current_chunk = [sentence]
-                        current_token_count = sentence_word_count
-                    else:
-                        current_chunk.append(sentence)
-                        current_token_count += sentence_word_count
-                if current_chunk:
-                    yield ' '.join(current_chunk)
-
-        src_sents = sent_tokenize_with_newlines(src)
-        src_sents = src_sents or ['']
-
-        src_chunks = [chunk.replace(" \n ", '\n').strip()
-                      for chunk in chunks(src_sents, max_chunk_size)]
-        src_chunks = [chunk for chunk in src_chunks if chunk != '']
-        return src_chunks
-
-    # todo: duplicated function, abstract it out to avoid two copies.
-    def chunking_src_mop(self, src: str, max_chunk_size: int = 550):  # 350
+    def chunking_src(self, src: str, max_chunk_size: int = 550): 
         def get_tokens(text: str) -> List[str]:
             return nltk.word_tokenize(text)
 
         def count_tokens(text: str) -> int:
-            # if "deberta" in self.model_name:
-            #     return len(self.tokenizer(
-            #             text, padding=False, add_special_tokens=False,
-            #             max_length=2048, truncation=True)['input_ids'])
-            # else:
             return len(get_tokens(text))
 
         def split_into_sentences(text: str, offset: int = 0) -> List[str]:
@@ -198,90 +130,6 @@ class Inferencer():
 
         return chunks
 
-    def chunking_src_mop_sliding_window(self, src: str, max_chunk_size: int = 400, max_step_size: int = 200):
-        def get_tokens(text: str) -> List[str]:
-            return nltk.word_tokenize(text)
-
-        def count_tokens(text: str) -> int:
-            return len(get_tokens(text))
-
-        def split_into_sentences(text: str, offset: int = 0) -> List[str]:
-            sentences: List[str] = sent_tokenize(text)
-            ret = []
-            idx = offset
-            len_of_text = len(text)
-            for sentence in sentences:
-                # increment idx until we match the character at idx to the first character of the sentence
-                first_char_of_sentence = sentence[0]
-                while idx < len_of_text and text[idx] != first_char_of_sentence:
-                    idx += 1
-                start = idx
-                end = idx + len(sentence)
-                tmp = {
-                    "text": sentence,
-                    "start": start,
-                    "end": end
-                }
-                idx = end + 1
-                ret.append(tmp)
-            return ret
-
-        def slide_window(sentences: List[str], max_step_size: int) -> List[str]:
-            step_size: int = 0
-            for i in range(len(sentences)):
-                s = sentences[i]
-                token_count = count_tokens(s["text"])
-                if step_size + token_count <= max_step_size:
-                    step_size += token_count
-                else:
-                    break
-            return sentences[i+1:]
-
-        def get_chunk(sentence: List[str], max_chunk_size: int) -> List[str]:
-            chunk_size: int = 0
-            chunk: str = ""
-            for s in sentences:
-                token_count = count_tokens(s["text"])
-                if chunk_size + token_count <= max_chunk_size:
-                    chunk = "\n".join([chunk, s["text"]]).strip('\n')
-                    chunk_size += token_count
-                else:
-                    break
-            return chunk.strip('\n')
-
-        sentences = split_into_sentences(src)
-        chunks: List[str] = []
-        while len(sentences) > 0:
-            chunk = get_chunk(sentences, max_chunk_size=max_chunk_size)
-            chunks.append(chunk)
-            sentences = slide_window(sentences, max_step_size=max_step_size)
-        return chunks
-
-    def chunking_src_textiling(self, src: str, max_chunk_size: int = 400):
-        def get_tokens(text: str) -> List[str]:
-            return nltk.word_tokenize(text)
-
-        def count_tokens(text: str) -> int:
-            return len(get_tokens(text))
-
-        try:
-            chunks: List[str] = []
-            if "\n\n" not in src and "\n" in src:
-                src = src.replace("\n", "\n\n")
-
-            chunks = self.textiling.tokenize(src)
-            chunks = [chunk.strip() for chunk in chunks]
-            for chunk in chunks:
-                if count_tokens(chunk) > 1500:
-                    return self.chunking_src_mop(src)
-                else:
-                    continue
-        except:
-            # print(src)
-            # chunks = [src]
-            return self.chunking_src_mop(src)
-        return chunks
-
     def inference(self, premise, hypo):
         """
         inference a list of premise and hypo
@@ -313,8 +161,7 @@ class Inferencer():
         input premise and hypos are lists
         """
         assert isinstance(premise, list) and isinstance(hypo, list)
-        assert len(premise) == len(
-            hypo), "premise and hypo should be in the same length."
+        assert len(premise) == len(hypo), "premise and hypo should be in the same length."
 
         batch = []
         for mini_batch_pre, mini_batch_hypo in zip(self.chunks(premise, self.batch_size), self.chunks(hypo, self.batch_size)):
@@ -347,12 +194,24 @@ class Inferencer():
                 )
         else:
             try:
-                output = self.tokenizer(premise_list, hypo_list, truncation='only_first',
-                                        padding='longest', max_length=2048, return_tensors='pt')
+                output = self.tokenizer(
+                    premise_list,
+                    hypo_list, 
+                    truncation='only_first',
+                    padding='longest', 
+                    max_length=2048, 
+                    return_tensors='pt'
+                )
             except:
                 warning('text_b too long...')
-                output = self.tokenizer(premise_list, hypo_list, truncation=True,
-                                        padding='longest', max_length=2048, return_tensors='pt')
+                output = self.tokenizer(
+                    premise_list, 
+                    hypo_list, 
+                    truncation=True,
+                    padding='longest', 
+                    max_length=2048, 
+                    return_tensors='pt'
+                )
 
         return output
 
